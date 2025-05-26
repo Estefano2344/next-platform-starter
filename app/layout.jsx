5c20afe3db8e139c90e38bb45f9f7bedb97c45de
Estefano Mateo Proaño Ingavelez <estefano.proano@epn.edu.ec>
2025-05-26 15:19:56 -0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,34 +1,33 @@
 import '../styles/globals.css';
 import { Footer } from '../components/footer';
 import { Header } from '../components/header';
+import Script from 'next/script';
 
 export const metadata = {
     title: {
         template: '%s | Netlify',
-        default: 'Netlify Starter',
-    },
+        default: 'Netlify Starter'
+    }
 };
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <head>
-                {/* Hotjar Tracking Code for Sitio 6416226 (falta el nombre) */}
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                            (function(h,o,t,j,a,r){
-                                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                                h._hjSettings={hjid:6416226,hjsv:6};
-                                a=o.getElementsByTagName('head')[0];
-                                r=o.createElement('script');r.async=1;
-                                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                                a.appendChild(r);
-                            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-                        `,
-                    }}
-                />
                 <link rel="icon" href="/favicon.svg" sizes="any" />
+                {/* Hotjar Tracking Code */}
+                <Script id="hotjar-init" strategy="afterInteractive">
+                    {`
+                        (function(h,o,t,j,a,r){
+                            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                            h._hjSettings={hjid:6416223,hjsv:6};
+                            a=o.getElementsByTagName('head')[0];
+                            r=o.createElement('script');r.async=1;
+                            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                            a.appendChild(r);
+                        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                    `}
+                </Script>
             </head>
             <body className="antialiased text-white bg-blue-900">
                 <div className="flex flex-col min-h-screen px-6 bg-noise sm:px-12">
